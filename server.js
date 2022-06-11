be32dcc656e476039d17ecbb01153b8df392e41b
Paulina <pau.jastrzab@gmal.com>
2022-06-11 16:04:38 +0200

    new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('C'est la vie');
+  res.end('Testowanie jest super SUPER!');
 }
 
 const server = http.createServer(requestListener);
